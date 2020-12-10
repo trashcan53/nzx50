ce03d88bc6eef92f3ae95c7649eb73cb21176f27
unknown <philipzf1@gmail.com>
2020-12-10 21:40:04 +1300

    tst 2 rawr xd

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-var FatalityRate = 0;
-var RecoveryRate = 0;
-var maxNewCase = 0;
-var newCaseData = [];
-
 const getData = () => 
 {
 	const fetchPromise = fetch('https://sheets.googleapis.com/v4/spreadsheets/1dhd1FU0VmBB0DnqCyM7X2VEpONIWf8MkNJcYklJG-WY/values/Sheet1',
@@ -15,7 +10,7 @@ const getData = () =>
 	const streamPromise = fetchPromise.then((response) => response.json());
 	
 	streamPromise.then((j) => console.log(j));
-
+	
 }
 
 window.addEventListener('load', (event) => {
